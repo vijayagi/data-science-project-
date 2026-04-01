12846c2be20360132a0ec3579e570af8b7b0d5a9
vijayagi <santhoshvijayagiri18@gmail.com>
2026-04-01 15:47:43 +0530

    updated backend code

---
+++ project 4(logstic ,Rand for)/backend.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle 
+import numpy as np
+
+app=FastAPI()
+class Transaction(BaseModel):
+    distance_from_home: float
+    ratio_to_median_purchase_price: float
+    used_pin_number: int
+    online_order: int
+
+with open("credit_card_scaler.pkl", "rb") as f:
+    scaler = pickle.load(f)
+
+with open("credit_card_model.pkl", "rb") as f:
+    classifier_lr = pickle.load(f)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Fraud Detection API"}
+
+@app.post("/predict/")
+def predict(transaction: Transaction):
+    transaction_array = np.array([transaction.distance_from_home, transaction.ratio_to_median_purchase_price, transaction.used_pin_number, transaction.online_order]).reshape(1, -1)
+    transaction_scaled = scaler.transform(transaction_array)
+    prediction = classifier_lr.predict(transaction_scaled)
+    probability = classifier_lr.predict_proba(transaction_scaled)[0][1]
+    return {"prediction": "Fraud" if prediction[0] == 1 else "Not Fraud", "probability": f'{probability*100:.2f}%'}
+
+
